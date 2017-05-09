239568b3ef7e06897361b1805a7a74621822450f
Kossskom <ya.kom345@yandex.ru>
2017-05-09 22:19:10 +0300

    Делаю лабу

---
+++ lab5/Lab5.cpp
@@ -0,0 +1,13 @@
+# define FIO  "Komarov K.S."  
+# define GR   "st.gr.wDay 633" 
+# define LR3  "lb. 3" 
+#include <stdio.h>
+#include <math.h>
+#include <conio.h>
+main()
+{
+	float I;
+	scanf ("%f",&I);
+	
+	
+}
